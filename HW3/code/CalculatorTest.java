3dc1ed6a11e0d66e7fcafc6fcd3a99d1e9008a2c
caelum02 <caelum02@snu.ac.kr>
2021-04-30 02:18:41 +0900

    infix-postfix 변환, postfix 계산 완성 TODO: 에러 처리, 알고리즘 정

--- HW3/code/CalculatorTest.java
+++ HW3/code/CalculatorTest.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Stack;
 
 public class CalculatorTest {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		while (true) {
@@ -13,15 +13,20 @@ public class CalculatorTest {
 
 				command(input);
 			} catch (Exception e) {
-				System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
+				System.out.println("입력이 잘못되었습니다. 오류 : " + e);
 			}
 		}
 	}
 
+
+	/**
+	 *
+	 * @param input
+	 */
 	public static void command(String input) {
 		try {
 			String infix = preprocess(input);
-
+//			System.out.println(infix);
 			Stack<Object> postfix_reversed = toPostfix(infix);
 			Stack<Object> postfix = new Stack<>(), printStack = new Stack<>();
 
@@ -32,8 +37,9 @@ public class CalculatorTest {
 			}
 
 			long result = calculate(postfix);
+			System.out.print(printStack.pop());
 			while(!printStack.isEmpty()) {
-				System.out.print(printStack.pop()); System.out.print(' ');
+				System.out.print(' '); System.out.print(printStack.pop());
 			} System.out.print("\n");
 
 			System.out.println(result);
@@ -70,6 +76,7 @@ public class CalculatorTest {
 	}
 
 	// (3+4)*100000-5*(3^(5+3*2-4*(10-9)))
+	// - 2749 / 3481 * - 59 + 7080
 
 	public static Stack<Object> toPostfix(String infix) throws Exception {
 		int depthParenthesis = 0, len = infix.length();
@@ -94,7 +101,11 @@ public class CalculatorTest {
 				isDigit = (i+1 < len) && isDigit(infix.charAt(i+1));
 
 				if (!isDigit) {
-					Long operand = Long.parseLong(numberBuilder.toString());
+					Object operand;
+					if(c!='z')
+						operand = Long.parseLong(numberBuilder.toString());
+					else
+						operand = 'z';
 					postfix.push(operand);
 
 					while (!operatorStack.isEmpty()) {
@@ -106,12 +117,15 @@ public class CalculatorTest {
 			else {
 				if (c == '(') {
 					if(i!=0 && !isOperator(infix.charAt(i-1)) && infix.charAt(i-1) != '(')
-
+						throw new Exception("Invalid parenthesis (");
 
 					depthParenthesis++;
 				}
 
 				else if (c == ')') {
+					if(i==0 || !(isDigit(infix.charAt(i-1)) || infix.charAt(i-1) == ')'))
+						throw new Exception("Invalid parenthesis )");
+
 					depthParenthesis--;
 					if (depthParenthesis < 0)
 						throw new Exception("Invalid Parentheses");
@@ -123,20 +137,24 @@ public class CalculatorTest {
 				}
 
 				else if (isOperator(c)) {
-					int depth = postParDepth.empty() ? -1 : postParDepth.peek();
-					while (!postfix.empty() && postfix.peek() instanceof Character && postParDepth.peek() >= depth) {
-						Character prevOp = (Character) postfix.pop();
-						depth = postParDepth.pop();
+//					System.out.println(c);
+					int prevDepth = -1;
+					char prevOp = '+';
+					while (!postfix.empty() && postfix.peek() instanceof Character && (Character) postfix.peek() != 'z') {
+						char topOp = (Character) postfix.peek();
+						int topDepth = postParDepth.peek();
 
-						if (depth < depthParenthesis || (depth == depthParenthesis && compareOp(prevOp, c))) {
-							operatorStack.push(prevOp);
-							opParDepth.push(depth);
-						} else {
-							postfix.push(prevOp);
-							postParDepth.push(depth);
-							break;
+						if(compareOp(prevOp, topOp, prevDepth, topDepth) && compareOp(topOp, c, topDepth, depthParenthesis)) {
+							operatorStack.push(topOp);
+							postfix.pop();
+							opParDepth.push(topDepth);
+							postParDepth.pop();
+							prevDepth = topDepth;
+							prevOp = topOp;
 						}
+						else break;
 					}
+
 					operatorStack.push(c);
 					opParDepth.push(depthParenthesis);
 				}
@@ -152,12 +170,28 @@ public class CalculatorTest {
 		if (depthParenthesis != 0)
 			throw new Exception("Invalid Parentheses");
 
+		// remove imaginary 0
+		Stack<Object> tmpStack = new Stack<>();
+		while(!postfix.empty()) {
+			Object top = postfix.pop();
+			if(!(top instanceof Character) || (char) top != 'z')
+				tmpStack.push(top);
+		}
+		while(!tmpStack.isEmpty()) {
+			postfix.push(tmpStack.pop());
+		}
+
 		return postfix;
 	}
 
 	// compare priority of operators
 	// true if c1 < c2
-	private static boolean compareOp(char c1, char c2) {
+	private static boolean compareOp(char c1, char c2, int d1, int d2) {
+//		System.out.printf("%c %c %d %d\n", c1, c2, d1, d2);
+		return d1 < d2 || (d1 == d2 && comparePriority(c1, c2));
+	}
+
+	private static boolean comparePriority(char c1, char c2) {
 		int p1 = priority(c1), p2 = priority(c2);
 		if (p1 < p2) return true;
 		else if (p1 == p2) {
@@ -183,21 +217,20 @@ public class CalculatorTest {
 			throw new Exception("Found invalid character in input");
 
 		replaceUnaryMinus(stringBuilder);
-//		parenthesize(stringBuilder);
 
 		return stringBuilder.toString();
 	}
-
-	private static void parenthesize(StringBuilder stringBuilder) {
-		stringBuilder.insert(0, '(');
-		stringBuilder.append(')');
-	}
+//
+//	private static void parenthesize(StringBuilder stringBuilder) {
+//		stringBuilder.insert(0, '(');
+//		stringBuilder.append(')');
+//	}
 
 	// 입력에 적절하지 않은 문자가 존재할 경우 true 리턴
 	private static boolean invalidChar(StringBuilder s){
 		for(int i=0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			if(!(isDigit(c) || (isOperator(c) && c != '~') || c == '(' || c==')'))
+			if(!(isDigit(c) && c != 'z' || (isOperator(c) && c != '~') || c == '(' || c==')'))
 				return true;
 		}
 
@@ -210,6 +243,7 @@ public class CalculatorTest {
 			char curr = s.charAt(i);
 			if (curr == '-' && (isOperator(prev) || prev == '(')) {
 				s.setCharAt(i, '~');
+				s.insert(i++, 'z');
 			}
 			prev = curr;
 		}
@@ -220,7 +254,8 @@ public class CalculatorTest {
 	}
 
 	private static boolean isDigit(char c) {
-		return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
+		// 10진수 혹은 imaginary zero z
+		return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == 'z';
 	}
 
 	private static void removeBlank(StringBuilder s) {
@@ -257,65 +292,8 @@ public class CalculatorTest {
 				throw new ArithmeticException("Invalid power operation");
 			return (long) Math.pow(a, b);
 		}
+
 		else throw new Exception("Invalid operation");
 	}
 
 }
-
-//	static Pattern ELEM_PATTERN = Pattern.compile("([0-9]+)(\\(.*\\))(\\^)((?![)0-9])-)(\\*)(/)(%)(\\+)((?=[)0-9])-)");
-//
-//enum OperatorType {
-//	ParenthesisType ("",1) {
-//		@Override
-//		public long calculate(long[] operands) {return operands[0];}
-//	},
-//	PowType ("^", 2){
-//		@Override
-//		public long calculate(long[] operands) {
-//			return (long) Math.pow(operands[0], operands[1]);
-//		}
-//	},
-//	MinusType ("~",3){
-//		@Override
-//		public long calculate(long[] operands) {return -operands[0];}
-//	},
-//	MultType ("\\*", 4){
-//		@Override
-//		public long calculate(long[] operands) {
-//			return operands[0] * operands[1];
-//		}
-//	},
-//	DivType ("/", 5){`
-//		@Override
-//		public long calculate(long[] operands) {
-//			return operands[0] / operands[1];
-//		}
-//	},
-//	ModType ("%", 6){
-//		@Override
-//		public long calculate(long[] operands) {
-//			return operands[0] % operands[1];
-//		}
-//	},
-//	Add ("\\+", 7){
-//		@Override
-//		public long calculate(long[] operands) {
-//			return operands[0] + operands[1];
-//		}
-//	},
-//	SubType ("-",8){
-//		@Override
-//		public long calculate(long[] operands) {
-//			return operands[0] - operands[1];
-//		}
-//	};
-//
-//	private OperatorType(String print, int idx) {
-//		printString = print;
-//		this.idx = idx;
-//	}
-//
-//	public abstract long calculate(long[] operands);
-//	public String patternString, printString;
-//	public int idx;
-//}
